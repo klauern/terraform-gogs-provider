8148bb55fa85dcfb494feac642106bf943745cd1
Nick Klauer <klauer@gmail.com>
2017-11-24 12:06:39 -0600

    add more tests for gogs client

--- gogs/gogs_api_test.go
+++ gogs/gogs_api_test.go
@@ -2,10 +2,22 @@ package gogs
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	gogs "github.com/gogits/go-gogs-client"
 )
+
+func TestGogsUserDelete(t *testing.T) {
+	client := gogs.NewClient("http://localhost:3000", "2a4a2f289bd334b943013affdb353a1de8687870")
+	if err := client.AdminDeleteUser("random"); err != nil {
+		if strings.Contains(err.Error(), "404") {
+			t.Log("User already deleted")
+		} else {
+			t.Fatal(err)
+		}
+	}
+}
 
 func TestGogsUserCreate(t *testing.T) {
 	client := gogs.NewClient("http://localhost:3000", "2a4a2f289bd334b943013affdb353a1de8687870")
@@ -21,4 +33,25 @@ func TestGogsUserCreate(t *testing.T) {
 
 	fmt.Printf("%#v", user)
 
+}
+
+func TestGogsUserEdit(t *testing.T) {
+	client := gogs.NewClient("http://localhost:3000", "2a4a2f289bd334b943013affdb353a1de8687870")
+	disabled := false
+	err := client.AdminEditUser("random", gogs.EditUserOption{
+		Active: &disabled,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestGogsUserRead(t *testing.T) {
+	client := gogs.NewClient("http://localhost:3000", "2a4a2f289bd334b943013affdb353a1de8687870")
+	user, err := client.GetUserInfo("random")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Printf("%#v", user)
 }
